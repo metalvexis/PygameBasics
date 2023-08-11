42d16a22c60bf76f95605e74a1c632d168310ee7
metalvexis <jp.saballegue@gmail.com>
2023-08-11 16:43:19 +0800

    controllable grid size

--- basic/basic_controls.py
+++ basic/basic_controls.py
@@ -10,7 +10,7 @@ MAX_FPS = 120
 BG_COLOR = (255, 255, 255)
 SCREEN_WIDTH = 1280
 SCREEN_HEIGHT = 720
-SCREEN = pygame.display.set_mode((1280, 720))
+SCREEN = pygame.display.set_mode((1280, 720), flags=SRCALPHA)
 SCREEN.fill(BG_COLOR)
 CLOCK = pygame.time.Clock()
 FONT = pygame.font.SysFont("Arial", 12)
@@ -33,15 +33,17 @@ def hud_debug(screen: pygame.Surface) -> None:
 def draw_board(
     row=10,
     col=10,
-    width=20,
-    height=20,
+    cell_width=20,
+    cell_height=20,
     color: Color = (255, 255, 255),
 ) -> pygame.Surface:
-    board_surface = pygame.Surface(size=(row * width, col * height))
+    board_w = col * cell_width
+    board_h = row * cell_height
+    board_surface = pygame.Surface(size=(board_w, board_h))
     board_surface.fill(BG_COLOR)
-    for x in range(row):
-        for y in range(col):
-            rect = pygame.Rect(x * width, y * height, width, height)
+    for x in range(col):
+        for y in range(row):
+            rect = pygame.Rect(x * cell_width, y * cell_height, cell_width, cell_height)
             pygame.draw.rect(board_surface, color, rect, 1)
 
     return board_surface
@@ -49,7 +51,7 @@ def draw_board(
 
 def main():
     board_row = 5
-    board_col = 5
+    board_col = 7
 
     while RUNNING:
         for event in pygame.event.get():
@@ -57,19 +59,17 @@ def main():
                 pygame.quit()
                 sys.exit()
             if event.type == pygame.KEYUP:
+                SCREEN.fill(BG_COLOR)
                 if event.key == pygame.K_PAGEUP:
                     board_row += 1
                     board_col += 1
                 if event.key == pygame.K_PAGEDOWN:
-                    if board_row - 1 > 0 and board_col - 1 > 0:
-                        SCREEN.fill(BG_COLOR)
-                        board_row -= 1
-                        board_col -= 1
+                    board_row -= 1 if board_row - 1 > 0 else 0
+                    board_col -= 1 if board_col - 1 > 0 else 0
 
         hud_debug(SCREEN)
 
         board = draw_board(row=board_row, col=board_col, color=(0, 0, 0))
-
         SCREEN.blit(board, ((SCREEN_WIDTH / 2) - (board.get_width() / 2), 50))
 
         CLOCK.tick(MAX_FPS)
